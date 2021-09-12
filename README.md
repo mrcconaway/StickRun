e05ada869eb143d9e639585f2ccf7faf256eb295
mrcconaway <mrcconaway@gmail.com>
2021-09-12 10:43:17 -0400

    Simple git / cpp practice
    Added a simple helloworld.cpp file so we can practice using git branches, git pulls, git commits, and git pushes.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # StickRun
  Not a dino run knock off
+
+###
+ To get a bit of practice using git for code-sharing, please add a "cout" statement in your respective functions and commit it to the repository.  
---
+++ helloworld.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+// prototypes
+void mrc();
+void wetdeer();
+void jarjar();
+void unraveledmadness();
+
+
+/*   
+// use 'cout' to print out your name  //
+*/
+int main(){
+
+    
+    mrc();
+    wetdeer();
+    jarjar();
+    unraveledmadness();
+    
+    return 0;
+}
+
+void mrc()
+{
+    // code goes here
+}
+
+void wetdeer()
+{
+    // code goes here
+}
+void jarjar()
+{
+    // code goes here
+}
+void unraveledmadness()
+{
+    // code goes here
+}
