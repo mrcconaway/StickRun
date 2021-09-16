78bdd5065c06aa59a148b352702199191cdb0e0d
mrcconaway <mrcconaway@gmail.com>
2021-09-16 13:15:10 -0400

    added a comment about possible changed to player.h vars

--- player.h
+++ player.h
@@ -16,6 +16,10 @@ public:
     void setVelocity(float x);
     void setAcceloration(float x);
 private:
+    // Note:
+    //  For our x and y positions, we might wanna use olc::vi2d position; which stores the x,y coordinate as a postion vector {x,y}
+    //  but we need to research how to properly use this type "olc::vic2d"
+    //  but again, maybe we just wanna use seperate x and y postion vars ... i think we can make it work. 
     float px = 25; 
     float py; // try to get the y position on the line
     float vy;
