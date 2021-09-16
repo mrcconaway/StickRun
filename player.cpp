ee152911e67ec1ff045476739d8a6294bfbac91e
MuntzHackerMan <spencerbdana@gmail.com>
2021-09-16 00:40:05 -0400

    Added Accessory and Mutator Functions

--- player.cpp
+++ player.cpp
@@ -7,3 +7,17 @@ float player::getCoordinateY(){
 float player::getCoordinateX(){
     return px;
 }
+float player::playerVelocity(){
+    return vy;
+}
+float player::playerAcceloration(){
+    return ay;
+}
+void player::setPlayerY(float x){
+    py = x;
+}
+void player::setVelocity(float x){
+    vy = x;
+}void player::setAcceloration(float x){
+    ay = x;
+}
--- player.h
+++ player.h
@@ -7,8 +7,15 @@ public:
     //accessor functions
     float getCoordinateY();
     float getCoordinateX();
-
+    float playerVelocity();
+    float playerAcceloration();
+    //mutator functions
+    void setPlayerY(float x);
+    void setVelocity(float x);
+    void setAcceloration(float x);
 private:
     float px = 25; 
     float py = ScreenHeight() * 0.75 + 1; // try to get the y position on the line
+    float vy;
+    float ay;
 };
